dbc5d41776d87b535b8536a75a799dbd813ab4c1
baxtiyorWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-01-05 15:46:11 +0500

    kelishamiz-admin successfully

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center w-full h-[100vh]">
+      <h1 className="text text-[50px]">Kelishamiz</h1>
+    </div>
+  );
+};
+
+export default App;
