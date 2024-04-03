db09da90f4b13988ab76493d536e255214bea3a1
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-04-03 06:56:43 +0500

    ok

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -9,7 +9,7 @@ export default function AddProduct() {
 
   return (
     <>
-      <h1 className="text font-poppins text-[33px] font-medium not-italic leading-normal tracking-[-0.33px] text-[#130F1E]"></h1>
+      <h1 className="text font-poppins  text-[33px] font-medium not-italic leading-normal tracking-[-0.33px] text-[#130F1E]"></h1>
       <div className="child-product">
         {searchParamValue.get("q") === "Yengil avtomobil" && <LightCar />}
         {searchParamValue.get("q") === "Yuk tashish va mahsus transport" && (
