d40af8035ac26138c3b5141296a15185a4673375
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-04-25 20:45:18 +0500

    ok

--- src/components/Categoriyes.jsx
+++ src/components/Categoriyes.jsx
@@ -6,15 +6,7 @@ import { Link } from "react-router-dom";
 import { getCategoriesRootLisId, getCategoriesRootList } from "../exports/api";
 import Loading from "./../ui/loading/Loading";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-// import required modules
-import { useState } from "react";
-
 const SubmenuComponent = ({ childCategories, chilId }) => {
-  console.log(chilId);
   const { data } = useQuery({
     queryKey: ["category"],
     queryFn: () => getCategoriesRootLisId(chilId),
@@ -38,48 +30,11 @@ export default function Categoriyes() {
     queryKey: ["category"],
     queryFn: () => getCategoriesRootList(),
   });
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
   if (error) return `Error: ${error}`;
 
   if (isLoading) return <Loading />;
-  const dataImages = data?.data?.content?.map((item) => item?.file);
 
-  console.log(dataImages);
-  const settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToScroll: 4,
-    slidesToShow: 4,
-    initialSlide: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
   return (
     <div className="slider-container mt-5">
       <Carousel
