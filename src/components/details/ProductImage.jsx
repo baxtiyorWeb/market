032e7898b0ff4dad4a70b242db8c30075402291e
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-05-02 11:51:29 +0500

    ok

--- src/components/details/ProductImage.jsx
+++ src/components/details/ProductImage.jsx
@@ -1,66 +1,89 @@
-// Import Swiper styles
-import { Carousel, Image } from "antd";
-import { useState } from "react";
+import { Carousel } from "antd";
+import { useEffect, useRef, useState } from "react";
 import "react-medium-image-zoom/dist/styles.css";
 import "./Product-details.css";
+
 export default function ProductImage({ data }) {
   const [imgGetIndex, setImgGetIndex] = useState(0);
+  const [width, setWidth] = useState(0);
+  const refs = useRef(null);
+
+  const handleThumbnailClick = (index) => {
+    setImgGetIndex(index);
+  };
+
+  useEffect(() => {
+    if (!refs.current) return;
+    const scrollWidth = refs.current.scrollWidth;
+    const childrenElementCount = refs.current.childElementCount;
+    const width = scrollWidth / childrenElementCount;
+    setWidth(width);
+  }, []);
 
   const SliderProduct = () => {
     const handleSlideChange = (current) => {
       setImgGetIndex(current);
     };
 
+    useEffect(() => {
+      if (!refs.current || !width) return;
+      const numOfThumb = Math.round(refs.current.offsetWidth / width);
+      refs.current.scrollLeft = Math.min(
+        imgGetIndex * width,
+        refs.current.scrollWidth - refs.current.offsetWidth,
+      );
+    }, [width, imgGetIndex]);
+
     return (
-      <>
-        <div className="product-page-img relative">
-          <div className="relative">
-            <div className="h-full ">
-              <Carousel
-                afterChange={handleSlideChange}
-                initialSlide={imgGetIndex}
-                // className="w-[686px]"
-                rootClassName="image-container"
-                draggable
-                effect="fade"
-                waitForAnimate
-                arrows
-              >
-                {data?.files?.map((item, index) => (
-                  <Image
-                    src={`data:image/png;base64,${item.file?.fileBase64}`}
-                    loading="eager"
-                    className="h-[400px] w-full select-none border bg-blue-500 object-center"
-                  />
-                ))}
-              </Carousel>
-              <div className="thumbnails flex w-full items-center justify-center">
-                {data?.files?.map((thumbnail, index) => (
-                  <div
-                    key={index}
-                    className={`h-[200px] w-[200px] ${
-                      imgGetIndex === index
-                        ? "h-[200px] w-[200px] border"
-                        : "h-[200px] w-[200px]"
-                    }`}
-                    onClick={() => setImgGetIndex(index)}
-                  >
-                    <img
-                      className="img-thumbs"
-                      src={`data:image/png;base64,${thumbnail.file?.fileBase64}`}
-                      alt={`Thumbnail ${index + 1}`}
-                    />
-                  </div>
-                ))}
-              </div>
+      <div className="product-page-img relative">
+        <div className="relative w-full">
+          <div className="h-full w-full rounded-2xl">
+            <Carousel
+              afterChange={handleSlideChange}
+              initialSlide={imgGetIndex}
+              draggable
+              effect="fade"
+              waitForAnimate
+              arrows
+            >
+              {data?.files?.map((item, index) => (
+                <img
+                  key={index}
+                  src={`data:image/png;base64,${item.file?.fileBase64}`}
+                  loading="eager"
+                  className="h-[450px] w-full select-none rounded-2xl bg-blue-500 object-cover"
+                />
+              ))}
+            </Carousel>
+            <div className="absolute right-3 top-3 inline-block w-[100px] cursor-text select-none rounded-full border bg-whiteTextColor p-2 text-center text-textColor">
+              {data?.files?.length} | {imgGetIndex + 1}
             </div>
           </div>
+          <div
+            ref={refs}
+            className="flex h-full w-[700px] items-center justify-start overflow-x-scroll rounded-2xl"
+          >
+            {data?.files?.map((item, index) => (
+              <img
+                key={index}
+                onClick={() => handleThumbnailClick(index)}
+                src={`data:image/png;base64,${item.file?.fileBase64}`}
+                loading="eager"
+                className={`m-5 h-[150px] w-[150px] select-none rounded-2xl object-cover p-1 ${
+                  imgGetIndex === index
+                    ? "border-bgColor scale-110 border-2 transition-all duration-150"
+                    : "border-2 "
+                }`}
+              />
+            ))}
+          </div>
         </div>
-      </>
+      </div>
     );
   };
+
   return (
-    <div className=" products-slide product-details relative  rounded-[10px] border">
+    <div className="products-slide product-details relative rounded-[10px]">
       <SliderProduct />
     </div>
   );
