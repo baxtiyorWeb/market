5f91935d922c8a120c1f6ac7e5fede4cd663e545
Baxtiyor Qurbonnazarov <baxtiyorqurbonnazarov33@gmail.com>
2024-05-21 21:56:07 +0500

    add filter get-api

--- src/data/export.jsx
+++ src/data/export.jsx
@@ -1,62 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
 import { Checkbox, Collapse, Input } from "antd";
 import React from "react";
+import { useParams } from "react-router-dom";
+import api from "../config/api/api";
 import "./style.css";
 const Exports = () => {
+  const { id } = useParams();
+  const getCategoryPropertyFilters = async () => {
+    const res = await api.get(`/category/get-filters/${id}`);
+    console.log(res.data?.data);
+    return res.data?.data;
+  };
+
+  const { data: getFilters, isLoading } = useQuery({
+    queryKey: ["category/get-filters", id],
+    queryFn: getCategoryPropertyFilters,
+  });
+
+  const content = getFilters?.map((item, index) => ({
+    key: index,
+    label: item?.property?.name,
+    children: (
+      <>
+        <Input placeholder="qidirish" className="mb-5 mt-1 h-10 border" />
+        <div className="flex w-full items-center justify-start ">
+          <Checkbox className="mr-3 " id={item?.property?.name} />
+          <label
+            htmlFor={item?.property?.name}
+            className="text cursor-pointer select-none text-pretty text-base"
+          >
+            {item?.property?.name}
+          </label>
+        </div>
+      </>
+    ),
+  }));
+
   const items = [
-    {
-      key: "1",
-      label: "Kompaniya",
-      children: (
-        <>
-          <Input placeholder="qidirish" className="mb-5 mt-1 h-10 border" />
-          <div className="flex w-full items-center justify-start ">
-            <Checkbox className="mr-3 " id="filter1" name="filter1" />
-            <label
-              htmlFor="filter1"
-              className="text cursor-pointer text-pretty text-base"
-            >
-              Artel
-            </label>
-          </div>
-          <div className="flex w-full items-center justify-start ">
-            <Checkbox className="mr-3" id="filter1" name="filter1" />
-            <label
-              htmlFor="filter1"
-              className="text cursor-pointer text-pretty text-base"
-            >
-              Apple
-            </label>
-          </div>
-          <div className="flex w-full items-center justify-start ">
-            <Checkbox className="mr-3" id="filter1" name="filter1" />
-            <label
-              htmlFor="filter1"
-              className="text cursor-pointer text-pretty text-base"
-            >
-              Asus
-            </label>
-          </div>
-          <div className="flex w-full items-center justify-start ">
-            <Checkbox className="mr-3" id="filter1" name="filter1" />
-            <label
-              htmlFor="filter1"
-              className="text cursor-pointer text-pretty text-base"
-            >
-              Artel
-            </label>
-          </div>
-          <div className="flex w-full items-center justify-start ">
-            <Checkbox className="mr-3" id="filter1" name="filter1" />
-            <label
-              htmlFor="filter1"
-              className="text cursor-pointer text-pretty text-base"
-            >
-              Microsoft
-            </label>
-          </div>
-        </>
-      ),
-    },
     {
       key: "2",
       label: "Brend ",
@@ -213,7 +193,7 @@ const Exports = () => {
         ghost
         collapsible="header"
         defaultActiveKey={1}
-        items={items}
+        items={content}
       />
     </div>
   );
