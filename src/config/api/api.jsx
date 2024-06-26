3a5e711c0ef2ef1ecb15cb6ac686f46fe6815fe4
baxtiyoeWeb <baxtiyorqurbonnazarov33@gmail.com>
2024-06-26 12:16:09 +0500

    kelishamiz.uz

--- src/config/api/api.jsx
+++ src/config/api/api.jsx
@@ -1,48 +1,48 @@
-// Separate refresh token function
-
 import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://95.130.227.131:8080/api/v1",
   headers: {
-    "ngrok-skip-browser-warning": true,
     "Content-Type": "application/json",
-    Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-    SecretKey: `${localStorage.getItem("secretKey")}`,
   },
-  // params: {
-  // lang: getParamsLangQuery(),
-  // },
 });
-async function refreshToken() {
-  try {
-    const refreshToken = localStorage.getItem("refreshToken");
-    const response = await axios.post("/authority/refresh-token", {
-      refreshToken,
-    });
-    if (response.status === 200) {
-      localStorage.setItem("accessToken", response.data?.data.accessToken);
-      return response.data?.data.accessToken;
-    }
-  } catch (error) {
-    console.error("Refresh token error", error);
-    // Handle error, potentially redirect to login
-  }
-}
 
-// Updated interceptor
+// Request interceptor
+api.interceptors.request.use(
+  async (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
+
+// Response interceptor
 api.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    return response;
+  },
   async (error) => {
-    if (error.response && error.response.status === 401) {
-      try {
-        const newAccessToken = await refreshToken();
-        if (newAccessToken) {
-          error.config.headers["Authorization"] = `Bearer ${newAccessToken}`;
-          return api(error.config);
-        }
-      } catch (refreshError) {
-        console.error("Refresh token error", refreshError);
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refreshToken = localStorage.getItem("refreshToken");
+      const response = await axios.post(
+        "http://95.130.227.131:8080/api/v1/authority-refresh-token",
+        {
+          refreshToken: refreshToken,
+        },
+      );
+      if (response.status === 200) {
+        const newAccessToken = response.data.accessToken;
+        localStorage.setItem("accessToken", newAccessToken);
+        api.defaults.headers.common["Authorization"] =
+          `Bearer ${newAccessToken}`;
+        return api(originalRequest);
       }
     }
     return Promise.reject(error);
